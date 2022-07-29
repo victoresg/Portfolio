4fba4dd03d4e22bf8727b0f2de7649a488fd0f4f
victor.elias <elias.victor@seedz.ag>
2022-07-29 16:57:16 -0300

    att.

--- src/components/layout/Header/styles.ts
+++ src/components/layout/Header/styles.ts
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-background-color: var(--primary);
+  background-color: var(--primary);
+  @media(max-width: 768px) {
+    position: fixed;
+    width: 100%;
+  }
   h1 {
     text-align: center;
   }
--- src/components/layout/Menu/styles.ts
+++ src/components/layout/Menu/styles.ts
@@ -3,27 +3,32 @@ import styled from "styled-components";
 export const Container = styled.nav`
     display: flex;
     justify-content: space-between;
-    padding: 15px 60px;
+    padding: 30px 60px;
+    height: auto;
   a {
-    &:hover {
-      /* background-color: red; */
-      position: relative;
-      &::before {
-        content: "";
-        border: 2px solid #000;
-        position: absolute;
-        left: -10px;
-        top: 0;
-        bottom: 0;
-      }
-      &::after {
-        content: "";
-        border: 2px solid #000;
-        position: absolute;
-        right: -10px;
-        top: 0;
-        bottom: 0;
-      }
+    text-decoration: none;
+    color: var(--dark);
+    font-weight: bold;
+    transition: all 0.3s;
+  }
+  .active {
+    transform: scale(1.1);
+    position: relative;
+    &::before {
+      content: "";
+      border: 2px solid #000;
+      position: absolute;
+      left: -10px;
+      top: 0;
+      bottom: 0;
+    }
+    &::after {
+      content: "";
+      border: 2px solid #000;
+      position: absolute;
+      right: -10px;
+      top: 0;
+      bottom: 0;
     }
   }
 `
