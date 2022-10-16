5d98430d9618989ca7dc97ee98cf72714878fe36
victoresg <victoresg9@gmail.com>
2022-10-16 19:25:58 -0300

    att.

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/victoresg.github.io">
       <App />
     </BrowserRouter>
   </React.StrictMode>
