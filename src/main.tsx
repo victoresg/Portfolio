bff079ab6152031b4c9b14869e1b1ee2e11cd9c2
victoresg <victoresg9@gmail.com>
2023-04-04 10:55:17 -0300

    att.

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/vite-deploy-demo">
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 const path = require('path');
 
 export default defineConfig({
-  base: '/',
+  base: '/vite-deploy-demo/',
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
